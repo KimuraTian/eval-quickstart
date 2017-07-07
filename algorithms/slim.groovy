78a28c58d8f1b336915b6d5eef5167cd2da5033d
MucunTian <tian.mucun@gmail.com>
2017-07-07 17:42:15 -0600

    eval configuration changed

--- algorithms/slim.groovy
+++ algorithms/slim.groovy
@@ -1,13 +1,15 @@
 import org.lenskit.baseline.BaselineScorer
 import org.lenskit.baseline.ItemMeanRatingItemScorer
 import org.lenskit.knn.item.ItemItemScorer
-import org.lenskit.slim.Epsilon
-import org.lenskit.slim.SLIMScorer
+import org.lenskit.knn.item.ModelSize
+import org.lenskit.slim.MinItemWeight
+import org.lenskit.slim.SLIMItemScorer
 import org.lenskit.transform.normalize.BaselineSubtractingUserVectorNormalizer
 import org.lenskit.transform.normalize.UserVectorNormalizer
 
-bind ItemScorer to SLIMScorer
-set Epsilon to 0.0001
+bind ItemScorer to SLIMItemScorer
+set MinItemWeight to 0.0001
+set ModelSize to 100
 bind UserVectorNormalizer to BaselineSubtractingUserVectorNormalizer
 within (UserVectorNormalizer) {
     bind (BaselineScorer, ItemScorer) to ItemMeanRatingItemScorer
