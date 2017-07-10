d9c36792d623d1905bd1a91c74e06c1f1ba25c6d
MucunTian <tian.mucun@gmail.com>
2017-07-10 16:25:44 -0600

    add basic slim eval configuration

--- algorithms/slim.groovy
+++ algorithms/slim.groovy
@@ -6,10 +6,13 @@ import org.lenskit.slim.MinItemWeight
 import org.lenskit.slim.SLIMItemScorer
 import org.lenskit.transform.normalize.BaselineSubtractingUserVectorNormalizer
 import org.lenskit.transform.normalize.UserVectorNormalizer
+import org.lenskit.slim.SLIMBuildContext
+import org.lenskit.slim.SLIMBuildContextProvider
 
 bind ItemScorer to SLIMItemScorer
-set MinItemWeight to 0.0001
-set ModelSize to 100
+set MinItemWeight to 0.00001
+//set ModelSize to 100
+bind SLIMBuildContext toProvider SLIMBuildContextProvider
 bind UserVectorNormalizer to BaselineSubtractingUserVectorNormalizer
 within (UserVectorNormalizer) {
     bind (BaselineScorer, ItemScorer) to ItemMeanRatingItemScorer
