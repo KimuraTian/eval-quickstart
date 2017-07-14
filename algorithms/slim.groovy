be2414f9a2748125bf7c85b55535652ba1a07d33
MucunTian <tian.mucun@gmail.com>
2017-07-13 18:17:34 -0600

    changed to holdout 1

--- algorithms/slim.groovy
+++ algorithms/slim.groovy
@@ -8,12 +8,22 @@ import org.lenskit.transform.normalize.BaselineSubtractingUserVectorNormalizer
 import org.lenskit.transform.normalize.UserVectorNormalizer
 import org.lenskit.slim.SLIMBuildContext
 import org.lenskit.slim.SLIMBuildContextProvider
+import org.lenskit.slim.RidgeRegressionTerm
+import org.grouplens.lenskit.iterative.StoppingCondition
+import org.grouplens.lenskit.iterative.ThresholdStoppingCondition
+import org.grouplens.lenskit.iterative.StoppingThreshold
+import org.grouplens.lenskit.iterative.RegularizationTerm
 
 bind ItemScorer to SLIMItemScorer
-set MinItemWeight to 0.00001
-//set ModelSize to 100
+set MinItemWeight to 0.000001
+set ModelSize to 100
 bind SLIMBuildContext toProvider SLIMBuildContextProvider
 bind UserVectorNormalizer to BaselineSubtractingUserVectorNormalizer
 within (UserVectorNormalizer) {
     bind (BaselineScorer, ItemScorer) to ItemMeanRatingItemScorer
 }
+bind StoppingCondition to ThresholdStoppingCondition
+set StoppingThreshold to 1.0e-2
+set RidgeRegressionTerm to 1
+set RegularizationTerm to 0.5
+
