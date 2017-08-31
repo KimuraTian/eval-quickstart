eedf733b5eddcde4eefc891d5d41b267e1fabf11
MucunTian <tian.mucun@gmail.com>
2017-08-31 14:31:27 -0600

    add svd compile

--- algorithms/funksvd-implicit.groovy
+++ algorithms/funksvd-implicit.groovy
@@ -14,11 +14,11 @@ import org.lenskit.mf.funksvd.FeatureCount
 import org.lenskit.mf.funksvd.FunkSVDItemScorer
 
 bind BiasModel to new GlobalBiasModel(0.0d)
+bind RatingVectorPDAO to EntityCountRatingVectorPDAO
+set InteractionEntityType to CommonTypes.RATING
 
 bind ItemScorer to FunkSVDItemScorer
 set IterationCount to 500
 
-bind RatingVectorPDAO to EntityCountRatingVectorPDAO
-set InteractionEntityType to CommonTypes.RATING
 
 set FeatureCount to 100
